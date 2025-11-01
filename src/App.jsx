ef348cf9fe57aa8a4bd7c5af00f7a9b21d04bb6f
JJFRENZY <jackjayeck@icloud.com>
2025-10-31 23:16:55 -0600

    week01

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { NewTodoForm } from "./NewTodoForm";
-import "./styles.css";
+import "/src/styles.css";
 import { TodoList } from "./TodoList";
 
 export default function App() {
